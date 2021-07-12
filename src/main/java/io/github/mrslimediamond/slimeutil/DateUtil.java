4225f06bbfcf30ca634a1e6687290b258e530e68
MrSlimeDiamond <findlayrichardson3@gmail.com>
2021-07-13 10:12:11 +1200

    Initial commit

---
+++ src/main/java/io/github/mrslimediamond/slimeutil/DateUtil.java
@@ -0,0 +1,22 @@
+package io.github.mrslimediamond.slimeutil;
+
+import java.time.Duration;
+import java.util.StringJoiner;
+
+public class DateUtil {
+    public static String formatDuration(final Duration d) {
+        final long days = d.toDays();
+        d = d.minusDays(days);
+        final long hours = d.toHours();
+        d = d.minusHours(hours);
+        final long minutes = d.toMinutes();
+        d = d.minusMinutes(minutes);
+        final long seconds = d.getSeconds();
+        final StringJoiner joiner = new StringJoiner(", ");
+        if (days > 0) joiner.add(days + " days");
+        if (hours > 0) joiner.add(hours + " hours");
+        if (minutes > 0) joiner.add(minutes + " minutes");
+        if (seconds > 0) joiner.add(seconds + " seconds");
+        return joiner.toString();
+    }
+}
